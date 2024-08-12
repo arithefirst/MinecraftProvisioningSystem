0da6ecee6f1498b340b1bc1bbd79e9109ecbc209
AriTheFirst <ari@arithefirst.com>
2024-08-12 01:50:23 -0400

    Switched to using maps instead of a bunch of vars to get the content from the various user inputs

--- frontend/randombullshitGO.js
+++ frontend/randombullshitGO.js
@@ -27,66 +27,93 @@ function truncate(inputString, searchString) {
 }
 
 function sendRequest() {
-  // Get the values of all of the switches
-  var allowFlight = $("#allowFlight").is(":checked");
-  var allowNether = $("#allowNether").is(":checked");
-  var broadcastConsoleOps = $("#broadcastConsoleOps").is(":checked");
-  var broadcastRconOps = $("#broadcastRconOps").is(":checked");
-  var enableCommandBlock = $("#enableCommandBlock").is(":checked");
-  var enableJmxMonitoring = $("#enableJmxMonitoring").is(":checked");
-  var enableRcon = $("#enableRcon").is(":checked");
-  var enableStatus = $("#enableStatus").is(":checked");
-  var enableQuery = $("#enableQuery").is(":checked");
-  var enforceSecureProfile = $("#enforceSecureProfile").is(":checked");
-  var enforceWhitelist = $("#enforceWhitelist").is(":checked");
-  var forceGamemode = $("#forceGamemode").is(":checked");
-  var generateStructures = $("#generateStructures").is(":checked");
-  var hardcore = $("#hardcore").is(":checked");
-  var hideOnlinePlayers = $("#hideOnlinePlayers").is(":checked");
-  var onlineMode = $("#onlineMode").is(":checked");
-  var preventProxyConnections = $("#preventProxyConnections").is(":checked");
-  var previewsChat = $("#previewsChat").is(":checked");
-  var pvp = $("#pvp").is(":checked");
-  var requireResourcePack = $("#requireResourcePack").is(":checked");
-  var snooperEnabled = $("#snooperEnabled").is(":checked");
-  var spawnAnimals = $("#spawnAnimals").is(":checked");
-  var spawnMonsters = $("#spawnMonsters").is(":checked");
-  var spawnNPCs = $("#spawnNPCs").is(":checked");
-  var syncChunkWrites = $("#syncChunkWrites").is(":checked");
-  var useNativeTransport = $("#useNativeTransport").is(":checked");
-  var whitelist = $("#whitelist").is(":checked");
+  // Put all of the values of the toggles in the "bool" map
+  const bools = new Map();
+  bools.set("AllowFlight", $("#allowFlight").is(":checked"));
+  bools.set("AllowNether", $("#allowNether").is(":checked"));
+  bools.set("BroadcastConsoleOps", $("#broadcastConsoleOps").is(":checked"));
+  bools.set("BroadcastRconOps", $("#broadcastRconOps").is(":checked"));
+  bools.set("EnableCommandBlock", $("#enableCommandBlock").is(":checked"));
+  bools.set("EnableJmxMonitoring", $("#enableJmxMonitoring").is(":checked"));
+  bools.set("EnableRcon", $("#enableRcon").is(":checked"));
+  bools.set("EnableStatus", $("#enableStatus").is(":checked"));
+  bools.set("EnableQuery", $("#enableQuery").is(":checked"));
+  bools.set("EnforceSecureProfile", $("#enforceSecureProfile").is(":checked"));
+  bools.set("EnforceWhitelist", $("#enforceWhitelist").is(":checked"));
+  bools.set("ForceGamemode", $("#forceGamemode").is(":checked"));
+  bools.set("GenerateStructures", $("#generateStructures").is(":checked"));
+  bools.set("Hardcore", $("#hardcore").is(":checked"));
+  bools.set("HideOnlinePlayers", $("#hideOnlinePlayers").is(":checked"));
+  bools.set("OnlineMode", $("#onlineMode").is(":checked"));
+  bools.set("PreventProxyConnections", $("#preventProxyConnections").is(":checked"));
+  bools.set("PreviewsChat", $("#previewsChat").is(":checked"));
+  bools.set("Pvp", $("#pvp").is(":checked"));
+  bools.set("RequireResourcePack", $("#requireResourcePack").is(":checked"));
+  bools.set("SnooperEnabled", $("#requireResourcePack").is(":checked"));
+  bools.set("SpawnAnimals", $("#spawnAnimals").is(":checked"));
+  bools.set("SpawnMonsters", $("#spawnMonsters").is(":checked"));
+  bools.set("SpawnNPCs", $("#spawnNPCs").is(":checked"));
+  bools.set("SyncChunkWrites", $("#syncChunkWrites").is(":checked"));
+  bools.set("UseNativeTransport", $("#useNativeTransport").is(":checked"));
+  bools.set("Whitelist", $("#whitelist").is(":checked"));
 
-  // Get the values of all of the text boxes
-  var difficulty = $("#difficulty").val();
-  var entityBroadcastRangePercent = $("#entityBroadcastRangePercent").val();
-  var funcPermLevel = $("#funcPermLevel").val();
-  var gamemode = $("#gamemode").val();
-  var funcPermLevel = $("#funcPermLevel").val();
-  var maxChainedNeighborUpdates = $("#maxChainedNeighborUpdates").val();
-  var maxPlayers = $("#maxPlayers").val();
-  var maxTickTime = $("#maxTickTime").val();
-  var maxWorldSize = $("#maxWorldSize").val();
-  var motd = $("#motd").val();
-  var networkCompressionThreshold = $("#networkCompressionThreshold").val();
-  var opPermLevel = $("#opPermLevel").val();
-  var playerIdleTimeout = $("#playerIdleTimeout").val();
-  var queryPort = $("#queryPort").val();
-  var rateLimit = $("#rateLimit").val();
-  var rconPassword = $("#rconPassword").val();
-  var rconPort = $("#rconPort").val();
-  var resourcePack = $("#resourcePack").val();
-  var resourcePackPrompt = $("#resourcePackPrompt").val();
-  var resourcePackSha1 = $("#resourcePackSha1").val();
-  var serverIP = $("#serverIP").val();
-  var serverPort = $("#serverPort").val();
-  var simDistance = $("#simulationDistance").val();
-  var spawnProtection = $("#spawnProtection").val();
-  var viewDistance = $("#viewDistance").val();
-  var levelName = $("#levelName").val();
-  var levelSeed = $("#levelSeed").val();
+  // Put all of the strings in the "strings" map
+  const strings = new Map();
+  strings.set("Difficulty", $("#difficulty").val());
+  strings.set("Gamemode", $("#gamemode").val());
+  strings.set("LevelName", $("#levelName").val());
+  strings.set("LevelSeed", $("#levelSeed").val());
+  strings.set("Motd", $("#motd").val());
+  strings.set("RconPassword", $("#rconPassword").val());
+  strings.set("ResourcePack", $("#resourcePack").val());
+  strings.set("ResourcePackPrompt", $("#resourcePackPrompt").val());
+  strings.set("ResourcePackSha1", $("#resourcePackSha1").val());
+  strings.set("ServerIP", $("#serverIP").val());
 
-  var jsonString = `{"AllowFlight": "${allowFlight}", "AllowNether": "${allowNether}", "BroadcastConsoleOps": "${broadcastConsoleOps}", "BroadcastRconOps": "${broadcastRconOps}", "Difficulty": "${difficulty}", "EnableCommandBlock": "${enableCommandBlock}", "EnableJmxMonitoring": "${enableJmxMonitoring}", "EnableRcon": "${enableRcon}", "EnableStatus": "${enableStatus}", "EnableQuery": "${enableQuery}", "EnforceSecureProfile": "${enforceSecureProfile}", "EnforceWhitelist": "${enforceWhitelist}", "EntityBroadcastRangePercent": ${entityBroadcastRangePercent}, "ForceGamemode": "${forceGamemode}", "FuncPermLevel": ${funcPermLevel}, "Gamemode": "${gamemode}", "GenerateStructures": "${generateStructures}", "Hardcore": "${hardcore}", "HideOnlinePlayers": "${hideOnlinePlayers}", "LevelName": "${levelName}", "LevelSeed": "${levelSeed}", "MaxChainedNeighborUpdates": ${maxChainedNeighborUpdates}, "MaxPlayers": ${maxPlayers}, "MaxTickTime": ${maxTickTime}, "MaxWorldSize": ${maxWorldSize}, "Motd": "${motd}", "NetworkCompressionThreshold": ${networkCompressionThreshold}, "OnlineMode": "${onlineMode}", "OpPermLevel": ${opPermLevel}, "PlayerIdleTimeout": ${playerIdleTimeout}, "PreventProxyConnections": "${preventProxyConnections}", "PreviewsChat": "${previewsChat}", "Pvp": "${pvp}", "QueryPort": ${queryPort}, "RateLimit": ${rateLimit}, "RconPassword": "${rconPassword}", "RconPort": ${rconPort}, "ResourcePack": "${resourcePack}", "ResourcePackPrompt": "${resourcePackPrompt}", "ResourcePackSha1": "${resourcePackSha1}", "RequireResourcePack": "${requireResourcePack}", "ServerIP": "${serverIP}", "ServerPort": ${serverPort}, "SimDistance": ${simDistance}, "SnooperEnabled": "${snooperEnabled}", "SpawnAnimals": "${spawnAnimals}", "SpawnMonsters": "${spawnMonsters}", "SpawnNPCs": "${spawnNPCs}", "SpawnProtection": ${spawnProtection}, "SyncChunkWrites": "${syncChunkWrites}", "UseNativeTransport": "${useNativeTransport}", "ViewDistance": ${viewDistance}, "Whitelist": "${whitelist}"}`;
+  // Put all of the ints in the "ints" map
+  const ints = new Map();
+  ints.set("EntityBroadcastRangePercent", $("#entityBroadcastRangePercent").val());
+  ints.set("FuncPermLevel", $("#funcPermLevel").val());
+  ints.set("MaxChainedNeighborUpdates", $("#maxChainedNeighborUpdates").val());
+  ints.set("MaxPlayers", $("#maxPlayers").val());
+  ints.set("MaxTickTime", $("#maxTickTime").val());
+  ints.set("MaxWorldSize", $("#maxWorldSize").val());
+  ints.set("NetworkCompressionThreshold", $("#networkCompressionThreshold").val());
+  ints.set("OpPermLevel", $("#opPermLevel").val());
+  ints.set("PlayerIdleTimeout", $("#playerIdleTimeout").val());
+  ints.set("QueryPort", $("#queryPort").val());
+  ints.set("RateLimit", $("#rateLimit").val());
+  ints.set("RconPort", $("#rconPort").val());
+  ints.set("ServerPort", $("#serverPort").val());
+  ints.set("SimDistance", $("#simulationDistance").val());
+  ints.set("SpawnProtection", $("#spawnProtection").val());
+  ints.set("ViewDistance", $("#viewDistance").val());
 
+  var jsonString = "{";
+
+  // Iterate through all of the bools
+  for (let [key, value] of bools) {
+    // "key": "value",
+    jsonString = jsonString + '"' + key + '"' + ": " + '"' + value + '", ';
+  }
+
+  // Iterate through all of the strings
+  for (let [key, value] of strings) {
+    // "key": "value",
+    jsonString = jsonString + '"' + key + '"' + ": " + '"' + value + '", ';
+  }
+
+  // Iterate through all of the ints
+  for (let [key, value] of ints) {
+    // "key": value,
+    jsonString = jsonString + '"' + key + '"' + ": " + value + ", ";
+  }
+
+  // Remove the last ", " and close the json with a "}"
+  jsonString = jsonString.slice(0, -2);
+  jsonString = jsonString + "}";
+
+  console.log(jsonString);
   const currentUrl = window.location.href;
   const baseUrl = currentUrl.split("?")[0];
 
