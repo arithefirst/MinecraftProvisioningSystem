a20fa18e90704337859a8defc117e63e4c4c5832
AriTheFirst <ari@arithefirst.com>
2024-08-30 15:48:23 -0400

    Added 1.21.1; Added handling for invalid fabric versions

--- CLI/main.go
+++ CLI/main.go
@@ -26,7 +26,7 @@ func main() {
 		"1.14.1", "1.14.2", "1.14.3", "1.14.4", "1.15", "1.15.1", "1.15.2", "1.16",
 		"1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5", "1.17", "1.17.1", "1.18",
 		"1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.19.4", "1.20",
-		"1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21"}
+		"1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.20.5", "1.20.6", "1.21", "1.21.1"}
 
 	if !slices.Contains(validVersions, os.Args[2]) {
 		panic("\"" + os.Args[2] + "\"" + " is not a valid Minecraft version.\nAll Minecraft versions above 1.12 are supported.\n(See https://minecraft.fandom.com/wiki/Java_Edition_version_history for more)")
@@ -58,6 +58,13 @@ func main() {
 			panic(cmdErr)
 		}
 
+	} else if strings.ToLower(os.Args[1]) == "fabric" {
+		invalidFabric := []string{"1.12", "1.12.1", "1.12.2", "1.13", "1.13.1"}
+		if slices.Contains(invalidFabric, os.Args[2]) {
+			panic("Err: Fabric only supports 1.14+")
+		} else {
+			// Fabric code here
+		}
 	}
 
 }
